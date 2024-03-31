f1d9cc3a9eee1a8a9a5c76d37c0daede0bb25155
jperaino <jim@spatiometrics.com>
2024-03-31 12:53:38 -0700

    add firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import "./App.css";
-import Login from "./components/firebase-vite-ui/Login";
+import LoginPage from "./components/firebase-vite-ui/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="min-w-screen min-h-screen">
-      <Login />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
