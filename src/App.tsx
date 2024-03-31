b703d4c6c2a71929ec81db05975ea7ce3d2b759b
jperaino <jim@spatiometrics.com>
2024-03-31 13:20:32 -0700

    add app skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,25 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RequireAuth from "./components/firebase-vite-ui/RequireAuth";
 import Dashboard from "./dashboard/Dashboard";
 import { AuthContextProvider } from "./components/firebase-vite-ui/AuthContext";
+import AppSkeleton from "./components/skeleton/AppSkeleton";
+import { TooltipProvider } from "./components/ui/tooltip";
 
 function App() {
   return (
     <div className="min-w-screen min-h-screen">
       <AuthContextProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route element={<RequireAuth />}>
-              <Route path="/" element={<Dashboard />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
+        <TooltipProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/login" element={<LoginPage />} />
+              <Route element={<RequireAuth />}>
+                <Route element={<AppSkeleton />}>
+                  <Route path="/" element={<Dashboard />} />
+                </Route>
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </TooltipProvider>
       </AuthContextProvider>
     </div>
   );
